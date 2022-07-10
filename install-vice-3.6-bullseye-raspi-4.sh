7ab2a2118ef9d8a00e1a69651f8d0f0fd305d4d3
erkrystof <15527821+erkrystof@users.noreply.github.com>
2022-07-10 14:13:45 -0500

    Create install-vice-3.6-bullseye-raspi-4.sh

---
+++ install-vice-3.6-bullseye-raspi-4.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+# This script does a rough install of VICE 3.6 on the RasPi 4, Bullseye Release
+# You can download and execute it directly via:
+# wget -O - https://raw.githubusercontent.com/erkrystof/vice/master/install-vice-3.6-raspi-4.sh | bash
+# or download manually:
+# wget https://raw.githubusercontent.com/erkrystof/vice/master/install-vice-3.6-raspi-4.sh
+# chmod +x install-vice-3.6-raspi-4.sh
+# ./install-vice-3.6-raspi-4.sh
+
+set -e
+
+log () {
+  echo ""
+  echo "****************************************"
+  echo "* $1"
+  echo "****************************************"
+  echo ""
+}
+
+log "Installing VICE 3.6 on RasPi 4"
+#log "Normal Raspi update/upgrade path"
+
+#sudo apt update
+#sudo apt full-upgrade
+
+log "SDL2 Dependencies (for kmsdrm)"
+
+#install dependencies for how we'll compile SDL2 and install the output package
+sudo apt update -y
+sudo apt full-upgrade -y
+
+sudo apt-get install -y lsb-release git dialog wget gcc g++ build-essential unzip xmlstarlet \
+  python3-pyudev ca-certificates libasound2-dev libudev-dev libibus-1.0-dev libdbus-1-dev \
+  fcitx-libs-dev libsndio-dev libx11-dev libxcursor-dev libxext-dev libxi-dev libxinerama-dev \
+  libxkbcommon-dev libxrandr-dev libxss-dev libxt-dev libxv-dev libxxf86vm-dev libgl1-mesa-dev \
+  libegl1-mesa-dev libgles2-mesa-dev libgl1-mesa-dev libglu1-mesa-dev libdrm-dev libgbm-dev \
+  devscripts debhelper dh-autoreconf libraspberrypi-dev libpulse-dev 
+
+log "Download VICE dependencies"
+
+#vice dependencies
+sudo apt install libmpg123-dev libpng-dev zlib1g-dev libasound2-dev libvorbis-dev libflac-dev \
+ libpcap-dev automake bison flex subversion libjpeg-dev portaudio19-dev texinfo xa65 dos2unix \
+ libsdl2-image-dev libsdl2-dev libsdl2-2.0-0 -y
+
+if [ -d ~/vice-src ]
+then
+    echo "Directory vice-src exists already.  Heading on in."
+else
+    mkdir ~/vice-src
+fi
+cd ~/vice-src
+
+log "Download VICE 3.6"
+
+wget -O vice-3.6.tar.gz https://sourceforge.net/projects/vice-emu/files/releases/vice-3.6.tar.gz/download
+tar xvfz vice-3.6.tar.gz
+
+log "Build VICE 3.6"
+cd vice-3.6
+./autogen.sh 
+
+ #change --prefix=<dir> if you want the binaries elsewhere
+./configure --prefix=/home/pi/vice-3.6 --enable-sdlui2 --without-oss --enable-ethernet \
+ --disable-catweasel --without-pulse --enable-x64 --disable-pdf-docs --with-fastsid
+make -j $(nproc)
+make install
