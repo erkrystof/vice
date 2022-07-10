4967462fefd35a394d58b2137d67a3d9f58266a6
erkrystof <15527821+erkrystof@users.noreply.github.com>
2022-07-10 14:51:05 -0500

    Update install-vice-3.6-bullseye-raspi-4.sh

--- install-vice-3.6-bullseye-raspi-4.sh
+++ install-vice-3.6-bullseye-raspi-4.sh
@@ -24,7 +24,14 @@ log "Installing VICE 3.6 on RasPi 4"
 sudo apt update -y
 sudo apt full-upgrade -y
 
-sudo apt-get install -y lsb-release git dialog wget gcc g++ build-essential 
+log "Install previous SDL local build dependencies - but may be needed for VICE compile"
+
+sudo apt-get install -y lsb-release git dialog wget gcc g++ build-essential unzip xmlstarlet \
+  python3-pyudev ca-certificates libasound2-dev libudev-dev libibus-1.0-dev libdbus-1-dev \
+  fcitx-libs-dev libsndio-dev libx11-dev libxcursor-dev libxext-dev libxi-dev libxinerama-dev \
+  libxkbcommon-dev libxrandr-dev libxss-dev libxt-dev libxv-dev libxxf86vm-dev libgl1-mesa-dev \
+  libegl1-mesa-dev libgles2-mesa-dev libgl1-mesa-dev libglu1-mesa-dev libdrm-dev libgbm-dev \
+  devscripts debhelper dh-autoreconf libraspberrypi-dev libpulse-dev
 
 log "Download VICE dependencies"
 
@@ -46,12 +53,20 @@ log "Download VICE 3.6"
 wget -O vice-3.6.tar.gz https://sourceforge.net/projects/vice-emu/files/releases/vice-3.6.tar.gz/download
 tar xvfz vice-3.6.tar.gz
 
-log "Build VICE 3.6"
+log "Autogen VICE"
 cd vice-3.6.0
 ./autogen.sh 
 
- #change --prefix=<dir> if you want the binaries elsewhere
+log "Configure VICE"
+ 
+#change --prefix=<dir> if you want the binaries elsewhere
 ./configure --prefix=/home/pi/vice-3.6 --enable-sdlui2 --without-oss --enable-ethernet \
  --disable-catweasel --without-pulse --enable-x64 --disable-pdf-docs --with-fastsid
+ 
+log "Make VICE"
+ 
 make -j $(nproc)
+
+log "Install VICE"
+
 make install
